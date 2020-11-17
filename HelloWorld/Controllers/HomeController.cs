8e81acce8c4daff2c8670a1b64c030745caa0a13
altansukh.p <altansukh.p@digitalworks.mn>
2020-11-17 13:44:45 +0800

    update cod 1

--- HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Drawing;
 
 namespace HelloWorld.Controllers
 {
@@ -11,8 +13,39 @@ namespace HelloWorld.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            ViewBag.Message = "This is my first MVC 5 App.";
+            decimal a = 0, b = 0, c;
+            c = a / b;
+
+            a = b / 0;
+
+			
+            for (int i = 0; i < 100; i++)
+            {
+                Image img = Image.FromFile("c:/test.png");
+                DataTable table = new DataTable();
+                table.Columns.Add("COL1");
+                table.Columns.Add("COL2");
+                table.Rows.Add(new object[] { "data1", "data2" });
+                table.Rows.Add(new object[] { "data1", "data2" });
+                table.Rows.Add(new object[] { "data1", "data2" });
+            }
+            ViewBag.Message = "This is my first MVC 5 App." + calcData(null);
             return View();
         }
+
+        private string calcData(DataRow dr)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                DataTable table = new DataTable();
+                table.Columns.Add("COL1");
+                table.Columns.Add("COL2");
+                table.Rows.Add(new object[] { "data1", "data2" });
+                table.Rows.Add(new object[] { "data1", "data2" });
+                table.Rows.Add(new object[] { "data1", "data2" });
+            }
+            return dr["COL"].ToString();
+        }
+        
     }
 }
